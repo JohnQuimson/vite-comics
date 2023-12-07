446de9cc9cbeac48c09c17dc77342f9fb8552234
JohnQuimson <john.quimson777@gmail.com>
2023-12-07 22:38:24 +0100

    feat: footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,6 +3,32 @@ import FooterTop from './FooterTop.vue';
 
 export default {
   name: 'Footer',
+  data() {
+    return {
+      socialIcons: [
+        {
+          foto: 'img/footer-facebook.png',
+          link: '#',
+        },
+        {
+          foto: 'img/footer-twitter.png',
+          link: '#',
+        },
+        {
+          foto: 'img/footer-youtube.png',
+          link: '#',
+        },
+        {
+          foto: 'img/footer-pinterest.png',
+          link: '#',
+        },
+        {
+          foto: 'img/footer-periscope.png',
+          link: '#',
+        },
+      ],
+    };
+  },
   components: {
     FooterTop,
   },
@@ -11,7 +37,25 @@ export default {
 
 <template>
   <FooterTop />
-  <footer>footer</footer>
+  <footer>
+    <div class="footer-sx">
+      <button>sign-up now!</button>
+    </div>
+    <div class="footer-dx">
+      <ul>
+        <li>
+          <a href="#">
+            <span>Follow us</span>
+          </a>
+        </li>
+        <li v-for="li in socialIcons">
+          <a :href="li.link">
+            <img :src="`../../public/${li.foto}`" alt="" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
 </template>
 
 <style scoped></style>
