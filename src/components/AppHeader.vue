7e6682e1fa2f93ae99334490485993a3a782d2e5
JohnQuimson <john.quimson777@gmail.com>
2023-12-07 19:08:14 +0100

    feat: add list navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,68 @@
 <script>
 export default{
     name: 'Header',
-}
+    data(){
+        return {
+            menu: [
+                {
+                    nome: 'characters',
+                    link: '#',
+                },
+                {
+                    nome: 'comics',
+                    link: '#',
+                },
+                {
+                    nome: 'movies',
+                    link: '#',
+                },
+                {
+                    nome: 'tv',
+                    link: '#',
+                },
+                {
+                    nome: 'games',
+                    link: '#',
+                },
+                {
+                    nome: 'collectibles',
+                    link: '#',
+                },
+                {
+                    nome: 'videos',
+                    link: '#',
+                },
+                {
+                    nome: 'fans',
+                    link: '#',
+                },
+                {
+                    nome: 'news',
+                    link: '#',
+                },
+                {
+                    nome: 'shop',
+                    link: '#',
+                },
+            ],
+        };
+    },
+};
 </script>
 
 <template>
-    <header>Header</header> 
+    <header>
+        <div class="logo">
+            <div class="cont-logo">
+                <img src="../../public/img/dc-logo.png" alt="">
+            </div>  
+        </div>
+        <ul class="navbar">
+            <li v-for="li in menu">
+                {{ li.nome }}
+            </li>
+        </ul>
+    </header> 
 </template>
 
 <style scoped lang="scss">
